8010c8c9c93472344715b1116b24c768c02e1ae3
Vladimir <valodya2008@gmail.com>
2022-02-18 16:36:05 +0200

    added developers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AccountModule } from './account/account.module';
+import { DevelopersModule } from './developers/developers.module';
 import { FilesModule } from './files/files.module';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
-    imports: [MoviesModule, AccountModule, FilesModule]
+    imports: [MoviesModule, AccountModule, FilesModule, DevelopersModule]
 })
 export class AppModule {}
