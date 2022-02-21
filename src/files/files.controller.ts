afb487bf3a0ee91a19e46c1272b48b49780184c2
Vladimir <valodya2008@gmail.com>
2022-02-21 13:05:59 +0200

    added upload files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, InternalServerErrorException, Post, Req, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { ApiConsumes } from '@nestjs/swagger';
+import { memoryStorage } from 'multer';
+import { FastifyRequest } from 'fastify';
 
 import { FilesService } from './files.service';
+import { FastifyFilesInterceptor, xlsFileFilter } from '../interseptors/fastifyFile.interseptor';
 
 @Controller('files')
 export class FilesController {
     constructor(private readonly filesService: FilesService) {}
 
-    @Get()
-    getFile() {
-        return this.filesService.showFile(); // will be fixed after microservice creating
+    @ApiConsumes('multipart/form-data')
+    @Post('upload-files')
+    @UseInterceptors(
+        FastifyFilesInterceptor('file', 10, {
+            storage: memoryStorage(),
+            fileFilter: xlsFileFilter
+        })
+    )
+    async multipleFileUpload(@Req() req: FastifyRequest, @UploadedFiles() files: Express.Multer.File[]) {
+        try {
+            const resData = await this.filesService.multipleFileUpload(files);
+            return resData;
+        } catch (e) {
+            console.error('multipleFileUpload err ---', e);
+            return new InternalServerErrorException('Error in sending data', e);
+        }
     }
 }
